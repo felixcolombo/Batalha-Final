0163e69019d3f6c7a1ec56cf287730a99b8cbc6f
felixcolombo <89814084+felixcolombo@users.noreply.github.com>
2022-01-14 07:16:42 -0300

    Escolher Personagens

--- Projeto01_MD02/src/com/github/felixcolombo/entidades/Personagens.java
+++ Projeto01_MD02/src/com/github/felixcolombo/entidades/Personagens.java
@@ -1,17 +1,49 @@
 package com.github.felixcolombo.entidades;
 
+import java.util.Scanner;
+
 public class Personagens {
+	
+	private static Scanner keyboard = new Scanner(System.in);
+	private static String personagem;
+	private static String personagemSelecionado;
+	private double poderPersonagem;
+	private double ataquePersonagem;
+	private double defesaPersonagem;
+	private double poderArma;
 	
 	public static void escolherPersonagem() {
 		
-		private String personagem;
-		private double poderPersonagem;
-		private double ataquePersonagem;
-		private double defesaPersonagem;
-		private double poderArma;
+		boolean erro;
 		
-		public Personagem () {
+		do {
+			erro=false;
+				
+			String menu = "\n";
+			menu += "    1 - Mago \n";
+			menu += "    2 - Lenhador \n";
+							
+			System.out.println(menu);
+			personagem= keyboard.next();
+				
+			if(!personagem.equals("1")&&!personagem.equals("2")) {
+				System.out.println("Oops!! Escolha inválida!");
+				System.out.println("Escolha entre as opções abaixo:\n");
+				erro=true;
+			}
+					
+		}while(erro);
+			
+		switch(personagem) {
+			case "1":
+				personagemSelecionado = "Mago";
+				break;
+			case "2":
+				personagemSelecionado = "Lenhador";
+				break;
+			}
 		
-		}
 	}
 }
+
+	
